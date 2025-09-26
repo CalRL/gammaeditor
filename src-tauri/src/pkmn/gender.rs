e13de2cb391c9ac8a877c1defe6f985382bd5fd9
Cal <43823915+CalRL@users.noreply.github.com>
2025-09-26 18:51:06 +0200

    Add gender.rs

--- src-tauri/src/pkmn/gender.rs
+++ src-tauri/src/pkmn/gender.rs
@@ -4,6 +4,36 @@ pub fn get_gender_from_enum(enum_str: &str) -> Option<&str> {
         "ENUM_Gender::NewEnumerator0" => Some("MALE"),
         "ENUM_Gender::NewEnumerator1" => Some("FEMALE"),
         "ENUM_Gender::NewEnumerator2" => Some("GENDERLESS"),
-        _ => Some("unknown"),
+        _ => None,
+    }
+}
+
+pub enum Gender {
+    Male,
+    Female,
+    Unknown
+}
+
+pub enum GenderStrings {
+}
+
+impl Gender {
+    pub fn from_str(string: &str) -> Gender {
+        match string.to_uppercase().as_str() {
+            "MALE" => Gender::Male,
+            "FEMALE" => Gender::Female,
+            _ => Gender::Unknown
+        }
+    }
+
+    /// Converts to UE 4/5 ENUM
+    pub fn as_enum(&self) -> String {
+        let string = match &self {
+            Gender::Male => "ENUM_Gender::NewEnumerator0",
+            Gender::Female => "ENUM_Gender::NewEnumerator1",
+            Gender::Unknown => "ENUM_Gender::NewEnumerator2"
+        };
+
+        string.to_string()
     }
 }
