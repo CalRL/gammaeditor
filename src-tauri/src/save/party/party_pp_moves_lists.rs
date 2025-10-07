e8bf4a4eacd3cdd71976ac15549a7c95088d0311
Cal <43823915+CalRL@users.noreply.github.com>
2025-10-07 17:57:31 +0200

    Party PP Moves + tests

--- src-tauri/src/save/party/party_pp_moves_lists.rs
+++ src-tauri/src/save/party/party_pp_moves_lists.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use gvas::properties::array_property::ArrayProperty;
 use gvas::properties::Property;
 use gvas::properties::struct_property::{StructProperty, StructPropertyValue};
@@ -19,35 +20,49 @@ impl PartyPPMovesLists {
     /// Returns moves index at
     /// Takes moves_array result
     pub fn moves_at(array: &ArrayProperty, index: usize) -> Option<&ArrayProperty> {
-        if let ArrayProperty::Structs { structs, .. } = &array {
-            let sp: &StructProperty = structs.get(index)?;
-            if let StructPropertyValue::CustomStruct { properties, .. } = &sp.value {
-                for (_k, v) in properties.0.iter() {
-                    if let Some(Property::ArrayProperty(arr)) = v.first() {
-                        return Some(arr);
-                    }
-                }
-            }
-        }
-        None
+        let structs = match array {
+            ArrayProperty::Structs { structs, .. } => structs,
+            _ => return None,
+        };
+
+        let sp = structs.get(index)?;
+        let properties = match &sp.value {
+            StructPropertyValue::CustomStruct { properties, .. } => &properties.0,
+            _ => return None,
+        };
+
+        properties
+            .values()
+            .find_map(|v| match v.first()? {
+                Property::ArrayProperty(arr) => Some(arr),
+                _ => None,
+            })
     }
 
     /// Gets max pp at an index
     /// Takes moves_at result
-    pub fn max_pp_at(moves: &ArrayProperty, index: usize) -> Option<i32> {
-        if let ArrayProperty::Structs { structs, .. } = &moves {
-            let sp: &StructProperty = structs.get(index)?;
-            if let StructPropertyValue::CustomStruct { properties, .. } = &sp.value {
-                for (k,v) in &properties.iter() {
-                    
-                    None
-                }
-            }
-        }
-        None
+    pub fn max_pp_at(moves: &ArrayProperty, index: usize) -> Option<&i32> {
+        let pp_struct = match &moves {
+            ArrayProperty::Structs { structs, .. } => structs.get(index)?,
+            _ => return None
+        };
+        let map = pp_struct.value.get_custom_struct()?.1;
+        let prop = map.iter()
+            .find(|(k,_)| k.starts_with("MaxPP"))
+            .and_then(|(_,v)| v.first())?;
+        Some(&prop.get_int()?.value)
     }
 
-    pub fn current_pp_at(moves: &ArrayProperty, index: usize) -> Option<i32> {
-
+    pub fn current_pp_at(moves: &ArrayProperty, index: usize) -> Option<&i32> {
+        let pp_struct = match &moves {
+            ArrayProperty::Structs { structs, .. } => structs.get(index)?,
+            _ => return None
+        };
+        let map = pp_struct.value.get_custom_struct()?.1;
+        // Typo "Curremt" is intentional, it's like that in the file...
+        let prop = map.iter()
+            .find(|(k,_)| k.starts_with("CurremtPP"))
+            .and_then(|(_,v)| v.first())?;
+        Some(&prop.get_int()?.value)
     }
 }
