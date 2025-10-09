2b50dc068a595233eb537b71a0f570b136e7bf55
Cal <43823915+CalRL@users.noreply.github.com>
2025-10-09 22:10:37 +0200

    Add helper functions

---
+++ src-tauri/src/save/pokemon/mod.rs
@@ -0,0 +1,6 @@
+pub mod attack_lists;
+pub mod pokemon_id;
+pub mod pokemon_info;
+pub mod pokemon_classes;
+pub mod shiny_list;
+pub mod pp_moves_lists;
---
+++ src-tauri/src/save/pokemon/pokemon_id.rs
@@ -0,0 +1,23 @@
+use gvas::GvasFile;
+use gvas::properties::array_property::ArrayProperty;
+use gvas::properties::Property;
+
+pub struct PokemonID;
+
+impl PokemonID {
+    /// Returns the ID ArrayProperty
+    pub fn id_array(property: &Property) -> Option<&ArrayProperty> {
+        property.get_array()
+    }
+
+    pub fn id_array_mut(property: &mut Property) -> Option<&mut ArrayProperty> {
+        property.get_array_mut()
+    }
+
+    pub fn id_at(array: &ArrayProperty, index: usize) -> Option<&i32> {
+        match array {
+            ArrayProperty::Ints { ints, .. } => ints.get(index),
+            _ => None
+        }
+    }
+}
