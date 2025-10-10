edfb2c32b7ed6a7e9c2776fc8f66cc6462afe5e0
Cal <43823915+CalRL@users.noreply.github.com>
2025-10-11 00:07:37 +0200

    pokemon_info work

--- src-tauri/src/save/pokemon/pokemon_info.rs
+++ src-tauri/src/save/pokemon/pokemon_info.rs
@@ -1,1 +1,66 @@
-pub struct PokemonInfo;
+use gvas::properties::array_property::ArrayProperty;
+use gvas::properties::Property;
+use gvas::properties::struct_property::{StructProperty, StructPropertyValue};
+use indexmap::IndexMap;
+
+struct CustomStruct<'a> {
+    key: &'a String,
+    value: &'a IndexMap<..>
+}
+
+pub fn get_struct_at_idx(property: &Property, idx: usize) -> Option<&StructProperty> {
+    let array = match property {
+        Property::ArrayProperty(prop) => {
+            prop
+        }
+        _ => return None
+    };
+
+    match array {
+        ArrayProperty::Structs { structs, .. } => {
+            structs.get(idx)
+        }
+        _ => None
+    }
+}
+
+pub fn get_info_at_idx_mut(property: &mut Property, idx: usize) -> Option<&mut ArrayProperty> {
+    todo!()
+}
+
+/// Returns a vector of properties ina StructProperty,
+/// In our case, it's usually a single object inside anyway
+// e.g.:
+// "CharacterIcon_32_B2BF2F66473512AEE610B29769F9E02F": [
+//      {
+//          "type": "ObjectProperty",
+//          "value": "None"
+//      }
+// ]
+fn get_starts_with<'a>(value: &'a StructProperty, string: &str) -> Option<&'a Vec<Property>> {
+    match &value.value {
+        StructPropertyValue::CustomStruct { properties, .. } => {
+            for (k,v) in properties.0.iter() {
+                if k.starts_with(string) {
+                    return Some(v)
+                }
+            }
+            None
+        }
+        _ => None
+    }
+}
+
+fn get_starts_with_mut<'a>(value: &'a mut StructProperty, string: &str) -> Option<&'a mut Vec<Property>> {
+    match &mut value.value {
+        StructPropertyValue::CustomStruct { ref mut properties, .. } => {
+            for (k,v) in properties.0.iter_mut() {
+                if k.starts_with(string) {
+                    return Some(v)
+                }
+            }
+            None
+        }
+        _ => None
+    }
+}
