017d72d808a723d565a22dbc58db69d8786d92b4
Cal <43823915+CalRL@users.noreply.github.com>
2025-10-12 17:02:40 +0200

    Update pokemon_classes.rs
    Remove get_parent and get_parent_mut, and therefore removed the tests.

--- src-tauri/src/save/pokemon/pokemon_classes.rs
+++ src-tauri/src/save/pokemon/pokemon_classes.rs
@@ -2,48 +2,41 @@ use gvas::GvasFile;
 use gvas::properties::array_property::ArrayProperty;
 use gvas::properties::Property;
 
+pub fn class_at(array: &ArrayProperty, idx: usize) -> Option<&String>{
+    let class_property = match &array {
+        ArrayProperty::Properties { properties, .. } => {
+            properties.get(idx)?
+        },
+        _ => return None
+    };
+
+    match &class_property {
+        Property::ObjectProperty(prop) => {
+            Some(&prop.value)
+        }
+        _ => None
+    }
+}
+
+/// Probably shouldn't be used, at least not until an enum for every class is written...
+pub fn class_at_mut(array: &mut ArrayProperty, idx: usize) -> Option<&mut String> {
+    let class_property: &mut Property = match array {
+        ArrayProperty::Properties { ref mut properties, .. } => {
+            properties.get_mut(idx)?
+        }
+        _ => return None
+    };
+
+    match class_property {
+        Property::ObjectProperty(ref mut prop) => {
+            Some(&mut prop.value)
+        },
+        _ => None
+    }
+}
+
 pub struct PokemonClasses;
 
 impl PokemonClasses {
-    /// Returns the parent property
-    pub fn get_parent(file: &GvasFile, key: String) -> Option<&Property> {
-        file.properties.get(&key)
-    }
 
-    pub fn get_parent_mut(file: &mut GvasFile, key: String) -> Option<&mut Property> {
-        file.properties.get_mut(&key)
-    }
-
-    pub fn class_at(array: &ArrayProperty, idx: usize) -> Option<&String>{
-        let class_property = match &array {
-            ArrayProperty::Properties { properties, .. } => {
-                properties.get(idx)?
-            },
-            _ => return None
-        };
-
-        match &class_property {
-            Property::ObjectProperty(prop) => {
-                Some(&prop.value)
-            }
-            _ => None
-        }
-    }
-
-    /// Probably shouldn't be used, at least not until an enum for every class is written...
-    pub fn class_at_mut(array: &mut ArrayProperty, idx: usize) -> Option<&mut String> {
-        let class_property: &mut Property = match array {
-            ArrayProperty::Properties { ref mut properties, .. } => {
-                properties.get_mut(idx)?
-            }
-            _ => return None
-        };
-
-        match class_property {
-            Property::ObjectProperty(ref mut prop) => {
-                Some(&mut prop.value)
-            },
-            _ => None
-        }
-    }
 }
--- src-tauri/tests/pokemon/pokemon_classes.rs
+++ src-tauri/tests/pokemon/pokemon_classes.rs
@@ -1,4 +1,4 @@
-use gammaeditor_lib::save::pokemon::pokemon_classes::PokemonClasses;
+use gammaeditor_lib::save::pokemon::pokemon_classes::{class_at};
 use crate::pokemon::pokemon_classes::common::get_gvas;
 
 #[path = "../common.rs"]
@@ -9,25 +9,9 @@ fn test_class_at_valid() {
     let gvas = get_gvas();
     let prop = gvas.properties.get("PartyPokemonClasses").expect("classes");
     let array = prop.get_array().expect("get array");
-    let val = PokemonClasses::class_at(&array, 0).unwrap();
+    let val = class_at(&array, 0).unwrap();
     // his name is misspelled in the game...
     assert!(val.contains("Metacross"));
-}
-
-#[test]
-fn test_get_parent_is_some() {
-    let gvas = get_gvas();
-
-    let val = PokemonClasses::get_parent(&gvas, "PartyPokemonClasses".to_string());
-    assert!(val.is_some())
-}
-
-#[test]
-fn test_get_parent_is_none() {
-    let gvas = get_gvas();
-
-    let val = PokemonClasses::get_parent(&gvas, "ThisDoesntExist".to_string());
-    assert!(val.is_none())
 }
 
 #[test]
@@ -35,6 +19,6 @@ fn test_class_at_out_of_bounds() {
     let gvas = get_gvas();
     let prop = gvas.properties.get("PartyPokemonClasses").expect("classes");
     let array = prop.get_array().expect("get array");
-    let val = PokemonClasses::class_at(&array, 99);
+    let val = class_at(&array, 99);
     assert!(val.is_none());
 }
