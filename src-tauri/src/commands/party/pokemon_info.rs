5c6211f816db332b8b79cd635da5a31f2886c3e9
Cal <43823915+CalRL@users.noreply.github.com>
2025-10-12 21:10:54 +0200

    specify types

--- src-tauri/src/commands/party/pokemon_info.rs
+++ src-tauri/src/commands/party/pokemon_info.rs
@@ -1,17 +1,21 @@
+use std::sync::RwLockReadGuard;
+use gvas::GvasFile;
+use gvas::properties::Property;
+use gvas::properties::struct_property::StructProperty;
 use tauri::App;
 use tauri_plugin_dialog::MessageDialogResult::No;
 use crate::property::traits::PropertyPath;
-use crate::save::{AppState, SharedState};
+use crate::save::{AppState, SharedGvas, SharedState};
 use crate::save::pokemon::pokemon_info as info;
 use crate::utils::custom_struct::get_struct_property_at_idx;
 
 #[tauri::command]
 fn get_name(app_state: AppState, index: usize) -> Option<String>{
-    let shared_gvas = app_state.gvas_file?;
-    let gvas = shared_gvas.read().ok()?;
+    let shared_gvas: SharedGvas = app_state.gvas_file?;
+    let gvas: RwLockReadGuard<GvasFile> = shared_gvas.read().ok()?;
 
-    let property = gvas.properties.get("PartyPokemonInfo")?;
-    let struct_prop = get_struct_property_at_idx(property, index)?;
-    let name = info::get_name(struct_prop)?.clone();
+    let property: &Property = gvas.properties.get("PartyPokemonInfo")?;
+    let struct_prop: &StructProperty = get_struct_property_at_idx(property, index)?;
+    let name: String = info::get_name(struct_prop)?.clone();
     Some(name)
 }
