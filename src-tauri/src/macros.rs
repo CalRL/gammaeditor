e514912f318698a0822518001e276a8121d49757
Cal <43823915+CalRL@users.noreply.github.com>
2025-10-13 18:01:02 +0200

    add macro

--- src-tauri/src/macros.rs
+++ src-tauri/src/macros.rs
@@ -11,3 +11,23 @@ macro_rules! gvas_mut {
         $s.gvas_file.as_ref()?.write().ok()?
     };
 }
+
+#[macro_export]
+macro_rules! gvas_struct_at {
+    ($app_state:expr, $key:expr, $index:expr) => {{
+        let gvas = gvas!($app_state);
+        let property = gvas.properties.get($key)?;
+        let struct_prop = get_struct_property_at_idx(property, $index)?;
+        struct_prop
+    }};
+}
+
+#[macro_export]
+macro_rules! gvas_struct_at_mut {
+    ($app_state:expr, $key:expr, $index:expr) => {{
+        let gvas = gvas_mut!($app_state);
+        let property = gvas.properties.get_mut($key)?;
+        let struct_prop = get_struct_property_at_idx_mut(property, $index)?;
+        struct_prop
+    }};
+}
