f48d6616b69f44a7ca86683d3e995b50d1aaae94
Cal <callumburnsoregan@gmail.com>
2025-11-08 20:02:31 +0100

    delete screen, replace with mod

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,13 +1,19 @@
 use egui::{Color32, Image, Vec2};
-use crate::ui::screen::render_pokemon_path;
+use crate::ui::image::ImageContainer;
 
 pub mod screen;
 pub mod menu;
-pub mod party_screen;
-pub(crate) mod single_screen;
+mod image;
 
 fn render_image<'a>(path: String) -> Image<'a> {
     Image::new(path)
+        .corner_radius(5)
+        .bg_fill(Color32::from_rgb(50, 50, 50))
+        .fit_to_exact_size(Vec2::new(64.0, 64.0))
+}
+
+fn render_image_container(container: &ImageContainer) -> Image {
+    Image::new(container.path.clone())
         .corner_radius(5)
         .bg_fill(Color32::from_rgb(50, 50, 50))
         .fit_to_exact_size(Vec2::new(64.0, 64.0))
