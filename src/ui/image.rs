1c88bf9233f27c09be14ad8aaf7d5d17a8bc26ee
Cal <callumburnsoregan@gmail.com>
2025-11-09 09:15:36 +0100

    add ImageCache

--- src/ui/image.rs
+++ src/ui/image.rs
@@ -1,7 +1,8 @@
-use egui::{Color32, Vec2};
+use std::collections::HashMap;
 use crate::save::pokemon::StorageType;
-use crate::ui::menu::OnClick;
 use crate::ui::screen::render_pokemon_path;
+use egui::{Color32, ColorImage, Context, TextureHandle, Vec2};
+use crate::app::Asset;
 
 #[derive(Clone)]
 pub struct ImageContainer {
@@ -31,4 +32,44 @@ pub struct ImageSettings {
     pub size: Option<Vec2>,
     pub bg_color: Option<Color32>,
     pub corner_radius: Option<f32>,
+}
+
+#[derive(Clone)]
+pub struct ImageCache {
+    pub map: HashMap<String, TextureHandle>,
+}
+
+impl ImageCache {
+    pub fn new() -> Self {
+        Self {
+            map: HashMap::new()
+        }
+    }
+
+    pub fn get(&mut self, ctx: &Context, path: &str) -> Option<&TextureHandle> {
+        if !self.map.contains_key(path) {
+            if let Some(texture) = Self::load_texture(ctx, path) {
+                self.map.insert(path.to_string(), texture);
+            }
+        }
+        self.map.get(path)
+    }
+
+    fn load_texture(ctx: &Context, path: &str) -> Option<TextureHandle> {
+        let file = Asset::get(path)?;
+        let img = image::load_from_memory(&file.data).ok()?;
+        let rgba = img.to_rgba8();
+        let (w, h) = (img.width() as usize, img.height() as usize);
+        let color_image = ColorImage::from_rgba_unmultiplied([w, h], &rgba);
+        Some(ctx.load_texture(path.to_owned(), color_image, egui::TextureOptions::LINEAR))
+    }
+
+    pub fn list_all(&self) -> String {
+        let keys = self.map.keys();
+        let mut list = String::new();
+        keys.for_each(|key| {
+            list.push_str(&format!("{} ", key));
+        });
+        list
+    }
 }
