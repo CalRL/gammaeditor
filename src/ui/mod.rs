a670c0f3e2c7b9c27fa3847551779fbf41c1f4b8
Cal <callumburnsoregan@gmail.com>
2025-11-09 09:16:10 +0100

    addrender_texture function

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,9 +1,12 @@
-use egui::{Color32, Image, Vec2};
+use egui::{Color32, Image, TextureHandle, Vec2};
+use egui::load::SizedTexture;
+use rust_embed::EmbeddedFile;
+use crate::app::Asset;
 use crate::ui::image::ImageContainer;
 
 pub mod screen;
 pub mod menu;
-mod image;
+pub(crate) mod image;
 
 fn render_image<'a>(path: String) -> Image<'a> {
     Image::new(path)
@@ -13,8 +16,22 @@ fn render_image<'a>(path: String) -> Image<'a> {
 }
 
 fn render_image_container(container: &ImageContainer) -> Image {
+    // let src = match Asset::get(container.path.clone().as_str()) {
+    //     None => {Image::new("")}
+    //     Some(image) => {
+    //         Image::new(image)
+    //     }
+    // };
     Image::new(container.path.clone())
         .corner_radius(5)
         .bg_fill(Color32::from_rgb(50, 50, 50))
         .fit_to_exact_size(Vec2::new(64.0, 64.0))
+}
+
+pub fn render_texture(texture: &TextureHandle) -> Image {
+    Image::new(SizedTexture {
+        id: texture.id(),
+        size: Vec2::new(64.0, 64.0),
+    }).corner_radius(5)
+        .bg_fill(Color32::from_rgb(50, 50, 50))
 }
