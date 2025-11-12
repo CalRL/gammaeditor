06d4d55c352264949e189289629aed42aa358acf
Cal <43823915+CalRL@users.noreply.github.com>
2025-11-12 21:09:46 +0100

    remove old generator

--- src/main.rs
+++ src/main.rs
@@ -22,17 +22,3 @@ fn main() {
     )
     .expect("How did we get here?");
 }
-
-fn run_generator(args: Vec<String>) -> Result<String, String> {
-    println!("Starting generator");
-
-    let output = process::Command::new("bin/generator.exe")
-        .args(args)
-        .output()
-        .map_err(|e| format!("Failed to run generator.exe: {e}"))?;
-
-    if !output.status.success() {
-        return Err(String::from_utf8_lossy(&output.stderr).to_string());
-    }
-    Ok(String::from_utf8_lossy(&output.stdout).to_string())
-}
