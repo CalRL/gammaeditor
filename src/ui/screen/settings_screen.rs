89341c3728a8ad67855ac9f07eb0ad40aefceebf
Cal <43823915+CalRL@users.noreply.github.com>
2025-11-20 12:19:31 +0100

    settings screen work

--- src/ui/screen/settings_screen.rs
+++ src/ui/screen/settings_screen.rs
@@ -19,18 +19,24 @@ impl SettingsScreen {
             log_contents: None,
         }
     }
+
+    pub fn load_contents(&mut self) -> Result<(), String> {
+        match fs::read_to_string(&self.log_path) {
+            Ok(content) => {
+                self.log_contents = Some(content);
+                Ok(())
+            }
+            Err(e) => {
+                let msg: String = format!("{}", e.to_string());
+                Logger::error(msg.clone());
+                Err(msg)
+            }
+        }
+    }
 }
 
 impl ScreenTrait for SettingsScreen {
     fn load(&mut self, app: &mut App) {
-        let file = match File::open(self.log_path.as_str()) {
-            Ok(f) => {f}
-            Err(_) => {
-                Logger::error("Failed to open log file...");
-                return;
-            }
-        };
-
         self.log_contents = match fs::read_to_string(&self.log_path) {
             Ok(content) => {Some(content)}
             Err(e) => {
@@ -38,7 +44,6 @@ impl ScreenTrait for SettingsScreen {
                 return;
             }
         };
-
     }
 
     fn ui(&mut self, ui: &mut Ui, app: &mut App) -> ScreenAction {
@@ -47,21 +52,32 @@ impl ScreenTrait for SettingsScreen {
 
         ui.add_space(5.0);
 
-        if let Some(logs) = &mut self.log_contents {
-            let header = CollapsingHeader::new("View logs");
-            header.show(ui, |ui| {
-                ScrollArea::both()
-                    .auto_shrink([false, false])
-                    .show(ui, |ui| {
-                        ui.add(
-                            TextEdit::multiline(logs)
-                                .code_editor()
-                                .interactive(false)
-                                .desired_width(f32::INFINITY)
-                                .frame(true)
-                        );
-                    });
+
+        let header = CollapsingHeader::new("Logs")
+            .show(ui, |ui| {
+                if ui.button("Refresh logs").clicked() {
+                    let res = self.load_contents();
+                    return ScreenAction::Reload
+                }
+
+                if let Some(ref mut logs) = self.log_contents {
+                    ScrollArea::both()
+                        .auto_shrink([true, true])
+                        .show(ui, |ui| {
+                            ui.add(
+                                TextEdit::multiline(logs)
+                                    .code_editor()
+                                    .interactive(false)
+                                    .desired_width(f32::INFINITY)
+                                    .frame(true)
+                            );
+                        });
+                }
+                ScreenAction::None
             });
+
+        if ui.button("Reload Image Cache").clicked() {
+            let res = app.reload_cache(ui.ctx());
         }
 
         action
